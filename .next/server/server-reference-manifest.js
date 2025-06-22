bc949f0a947930543625172ec4af587640894bcd
swaigwar <swaiggyverse@gmail.com>
2025-06-22 14:52:21 +0000

    sAIgTV Big Bang Review

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"WHR1CdqyEXri+21XR9T1Q1Ql/vLd/5yu6FDvlffEJXM=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
